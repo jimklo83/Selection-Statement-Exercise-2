c9fb3583a9df23da75a67962faaf61740bca5a51
jimklo83 <jimklo83@mail.com>
2023-10-02 14:51:51 -0500

    Added 'ToLower' method

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -5,35 +5,28 @@
         static void Main(string[] args)
         {
             Console.Write("What is your favorite school subject? ");
-            string subject = Console.ReadLine();
+            var subject = Console.ReadLine();
+            var cleanUp = subject.ToLower();
 
-            switch (subject)
+
+            switch (cleanUp)
             {
-                case "Math":
                 case "math":
                     Console.WriteLine("Math:\nMath sucks!");
                     break;
-                case "Science":
                 case "science":
                     Console.WriteLine("Science:\nIt's amazing what gets passed as science these days...");
                     break;
-                case "History":
                 case "history":
-                    Console.WriteLine("History\nReal history is interesting!!!");
+                    Console.WriteLine("History:\nReal history is interesting!!!");
                     break;
-                case "English":
                 case "english":
-                    Console.WriteLine("English\nEnglish is always a good skill to know.");
+                    Console.WriteLine("English:\nEnglish is always a good skill to know.");
                     break;
-                case "Gym":
                 case "gym":
-                case "P.E.":
                 case "p.e.":
-                case "PE":
                 case "pe":
-                case "Physical Education":
                 case "physical education":
-                case "Physical Ed":
                 case "physical ed":
                     Console.WriteLine("Physical Education:\nIt's good to get exercise at school.");
                     break;
