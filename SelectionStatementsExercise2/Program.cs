4d06c143f0c1c4b4e64b294f07db7a691f305194
jimklo83 <jimklo83@mail.com>
2023-10-02 13:41:14 -0500

    Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("What is your favorite school subject? ");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                case "math":
+                    Console.WriteLine("Math:\nMath sucks!");
+                    break;
+                case "Science":
+                case "science":
+                    Console.WriteLine("Science:\nIt's amazing what gets passed as science these days...");
+                    break;
+                case "History":
+                case "history":
+                    Console.WriteLine("History\nReal history is interesting!!!");
+                    break;
+                case "English":
+                case "english":
+                    Console.WriteLine("English\nEnglish is always a good skill to know.");
+                    break;
+                case "Gym":
+                case "gym":
+                case "P.E.":
+                case "p.e.":
+                case "PE":
+                case "pe":
+                case "Physical Education":
+                case "physical education":
+                case "Physical Ed":
+                case "physical ed":
+                    Console.WriteLine("Physical Education:\nIt's good to get exercise at school.");
+                    break;
+                default: Console.WriteLine("Error");
+                    break;
+            }
         }
     }
 }
